68dd11b8e5a48d9357a35f2c25ff107f7dfdaa57
Samuel33rc <samuel.renecorail@gmail.com>
2023-04-25 16:02:48 +0200

    updated backmarket and url for tasteDive

--- cypress/e2e/backmarcket.cy.js
+++ cypress/e2e/backmarcket.cy.js
@@ -18,15 +18,27 @@ describe('registration should pass', () => {
   })
 })
 
-describe('registration should fail because user already exist', () => {
-  it('registration', () => {
+describe('registration', () => {
+  it('wrong email registration', () => {
     cy.visit('https://preprod.backmarket.fr/register')
     cy.get('button:first').click()
-    cy.get('#firstName').type('Albert')
-    cy.get('#lastName').type('Brenaman')
-    cy.get('#signup-email').type('alber')
-    cy.get('#signup-password').type('Albert1234')
+    cy.get('#firstName').type('Jean')
+    cy.get('#lastName').type('Valjean')
+    cy.get('#signup-email').type('jeanval')
+    cy.get('#signup-password').type('Jvlajean1234')
     cy.get('[data-qa="signup-submit-button"]').click()
+    cy.url().should('include', 'dashboard/orders')
+  })
+})
+
+
+describe('login', () => {
+  it('login', () => {
+    cy.visit('https://preprod.backmarket.fr/register')
+    cy.get('button:first').click()
+    cy.get('#signin-email').type('jeanval@email.com')
+    cy.get('#signin-password').type('Jvlajean1234')
+    cy.get('[data-qa="signin-submit-button"]').click()
     cy.url().should('include', 'dashboard/orders')
   })
 })
--- cypress/e2e/tastediveApi.cy.js
+++ cypress/e2e/tastediveApi.cy.js
@@ -1,9 +1,6 @@
-const { param } = require("cypress/types/jquery")
 
 describe('TasteDive test suite', () => {
     it('get data', () => {
-        cy.request({
-        url: `https://tastedive.com/api/similar`
-        })
+        cy.request('https://reqres.in/api/users')
     })
 })
