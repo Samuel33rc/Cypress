73cc4aa78e76608da804e38c36beec6c85474d0c
Samuel33rc <samuel.renecorail@gmail.com>
2023-04-25 12:28:29 +0200

    Initial commit

--- cypress.config.js
+++ cypress.config.js
@@ -1,8 +1,36 @@
 const { defineConfig } = require("cypress");
 
+const execa = require('execa')
+const findBrowser = () => {
+  // the path is hard-coded for simplicity
+  const browserPath =
+    '/Applications/Brave Browser.app/Contents/MacOS/Brave Browser'
+
+  return execa(browserPath, ['--version']).then((result) => {
+    // STDOUT will be like "Brave Browser 77.0.69.135"
+    const [, version] = /Brave Browser (\d+\.\d+\.\d+\.\d+)/.exec(result.stdout)
+    const majorVersion = parseInt(version.split('.')[0])
+
+    return {
+      name: 'Brave',
+      channel: 'stable',
+      family: 'chromium',
+      displayName: 'Brave',
+      version,
+      path: browserPath,
+      majorVersion,
+    }
+  })
+}
+
 module.exports = defineConfig({
   e2e: {
     setupNodeEvents(on, config) {
+      return findBrowser().then((browser) => {
+        return {
+          browsers: config.browsers.concat(browser),
+        }
+      })
       // implement node event listeners here
     },
   },
